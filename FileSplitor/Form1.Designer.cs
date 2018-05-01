7a6f1aed1052d94fed4170673451a5f0a9296ea3
sunliang <ofts7th@163.com>
2018-05-01 14:54:31 +0800

    Add files

---
+++ FileSplitor/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace FileSplitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblMerMsg = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(342, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Size:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "MB";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Split";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(471, 375);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.lblMessage);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(463, 349);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Split";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(22, 157);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(47, 12);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Welcome";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblMerMsg);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(463, 349);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Merge";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Folder";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(344, 21);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(369, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(369, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Merge";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lblMerMsg
+            // 
+            this.lblMerMsg.AutoSize = true;
+            this.lblMerMsg.Location = new System.Drawing.Point(19, 96);
+            this.lblMerMsg.Name = "lblMerMsg";
+            this.lblMerMsg.Size = new System.Drawing.Size(47, 12);
+            this.lblMerMsg.TabIndex = 3;
+            this.lblMerMsg.Text = "Welcome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 399);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileSplitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblMerMsg;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ FileSplitor/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSplitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = this.openFileDialog1.FileName;
+                FileInfo fi = new FileInfo(this.textBox1.Text);
+                this.showSplitMessage("Size: " + Math.Round((double)fi.Length / (1024 * 1024), 2));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textBox1.Text))
+                return;
+            if (string.IsNullOrEmpty(this.textBox2.Text))
+                return;
+
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(this.SplitFile);
+            Thread thread = new Thread(ts);
+            thread.IsBackground = true;
+            thread.Start(new Tuple<string, string>(this.textBox1.Text, this.textBox2.Text));
+        }
+
+        private void showSplitMessage(string msg)
+        {
+            if (this.lblMessage.InvokeRequired)
+            {
+                this.Invoke(new Action<Label, string>(setText), this.lblMessage, msg);
+            }
+            else
+            {
+                this.setText(this.lblMessage, msg);
+            }
+        }
+
+        private void showMergeMessage(string msg)
+        {
+            if (this.lblMerMsg.InvokeRequired)
+            {
+                this.Invoke(new Action<Label, string>(setText), this.lblMerMsg, msg);
+            }
+            else
+            {
+                this.setText(this.lblMerMsg, msg);
+            }
+        }
+
+        private void setText(Label lbl, string msg)
+        {
+            lbl.Text = msg;
+        }
+
+        private void SplitFile(object obj)
+        {
+            Tuple<string, string> arg = (Tuple<string, string>)obj;
+            string filePath = arg.Item1;
+            long pieceSize = long.Parse(arg.Item2) * 1024 * 1024;
+            var fs = File.Open(filePath, FileMode.Open);
+            long fileLength = fs.Length;
+            //FileInfo fi = new FileInfo(filePath);
+            int fileCount = (int)Math.Ceiling((double)fileLength / pieceSize);
+            int fileIdx = 0;
+            while (fileIdx < fileCount)
+            {
+                string newFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "-" + (fileIdx + 1) + Path.GetExtension(filePath));
+
+                long startPos = fileIdx * pieceSize;
+                long endPos = startPos + pieceSize;
+                if (endPos > fileLength)
+                {
+                    endPos = fileLength;
+                }
+                int maxLength = 1024 * 1024;
+                long pos = startPos;
+                var ws = File.OpenWrite(newFilePath);
+
+                while (pos < endPos)
+                {
+                    this.showSplitMessage("Processing file " + (fileIdx + 1) + " of " + fileCount + ", progress: " + Math.Round((double)(pos - startPos) / (endPos - startPos), 2));
+
+                    int length = (int)(endPos - pos > maxLength ? maxLength : endPos - pos);
+                    byte[] bytes = new byte[length];
+                    fs.Position = pos;
+                    fs.Read(bytes, 0, length);
+                    ws.Position = pos - startPos;
+                    ws.Write(bytes, 0, length);
+                    ws.Flush();
+
+                    pos += length;
+                }
+
+                ws.Close();
+                fileIdx++;
+            }
+
+            fs.Close();
+            this.showSplitMessage("Finished");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox3.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textBox3.Text))
+                return;
+
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(this.MergeFile);
+            Thread thread = new Thread(ts);
+            thread.IsBackground = true;
+            thread.Start(this.textBox3.Text);
+        }
+
+        private void MergeFile(object obj)
+        {
+            string folderPath = obj.ToString();
+            string[] files = Directory.GetFiles(folderPath);
+            if (files.Length == 0)
+                return;
+
+            string firstFileName = files[0];
+            string fn = Path.GetFileNameWithoutExtension(firstFileName);
+            fn = fn.Substring(0, fn.LastIndexOf('-'));
+            fn = fn + Path.GetExtension(firstFileName);
+            fn = Path.Combine(Path.GetDirectoryName(firstFileName), fn);
+
+            int fileCount = files.Length;
+            int fileIdx = 0;
+            var ws = File.OpenWrite(fn);
+            long writePos = 0;
+
+            while (fileIdx < fileCount)
+            {
+                string filePath = files[fileIdx];
+                var fs = File.Open(filePath, FileMode.Open);
+                long fileLength = fs.Length;
+                int maxLength = 1024 * 1024;
+                long pos = 0;
+                while (pos < fileLength)
+                {
+                    this.showMergeMessage("Processing file " + (fileIdx + 1) + " of " + fileCount + ", progress: " + Math.Round((decimal)(pos) / fileLength, 2));
+
+                    int length = (int)(fileLength - pos > maxLength ? maxLength : fileLength - pos);
+                    byte[] bytes = new byte[length];
+                    fs.Position = pos;
+                    fs.Read(bytes, 0, length);
+                    ws.Position = writePos;
+                    ws.Write(bytes, 0, length);
+                    ws.Flush();
+
+                    pos += length;
+                    writePos += length;
+                }
+                fs.Close();
+                fileIdx++;
+            }
+
+            ws.Close();
+            this.showMergeMessage("Finished");
+        }
+    }
+}
